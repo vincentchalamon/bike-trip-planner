aab8ab53a270023a2474bb502304d26275ec23d7
Vincent Chalamon <407859+vincentchalamon@users.noreply.github.com>
2026-03-30 21:19:24 +0200

    fix: fix review from Vincent

--- api/config/packages/security.php
+++ api/config/packages/security.php
@@ -28,9 +28,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             ],
         ],
         'access_control' => [
-            ['path' => '^/docs', 'roles' => 'PUBLIC_ACCESS'],
             ['path' => '^/auth/(request-link|refresh|verify)$', 'roles' => 'PUBLIC_ACCESS'],
-            ['path' => '^/auth/logout', 'roles' => 'IS_AUTHENTICATED_FULLY'],
             ['path' => '^/', 'roles' => 'IS_AUTHENTICATED_FULLY'],
         ],
     ]);
